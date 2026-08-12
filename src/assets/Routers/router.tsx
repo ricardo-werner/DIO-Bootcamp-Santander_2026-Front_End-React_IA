dff8b053f792f51d51e021373d7a65f49df38be2
Ricardo Werner <wernerdive@gmail.com>
2026-08-12 10:33:44 -0300

    feat(router): configure application routes with React Router

--- src/assets/Routers/router.tsx
+++ src/assets/Routers/router.tsx
@@ -1,20 +1,25 @@
-import { createBrowserRouter } from 'react-router-dom'; 
+import { createBrowserRouter } from "react-router-dom";
 
-export const router = createBrowserRouter ([
+import { RootLayout } from "./components/layout/RootLayout";
+import { SimulationFormPage } from "./pages/SimulationFormPage";
+import { SimulationResultsPage } from "./pages/SimulationResultsPage";
+
+export const router = createBrowserRouter([
   {
+    element: <RootLayout />,
     children: [
       {
-        path: '/',
-          element: <h1>Formulário de Simulação</h1>,
+        path: "/",
+        element: <SimulationFormPage />,
       },
       {
-        path: '/resultado',
-        element: <h1>Resultado da Simulação</h1>
+        path: "/resultado/:id",
+        element: <SimulationResultsPage />,
       },
       {
-        path: '/historico',
-        element: <h1>Histórico de Simulações</h1>
-      }
-    ]
-  }
-])
+        path: "/historico",
+        element: <h1>Histórico de Simulações</h1>,
+      },
+    ],
+  },
+]);
