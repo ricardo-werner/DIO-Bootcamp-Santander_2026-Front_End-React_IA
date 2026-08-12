e19f4a529a650c4963339210242c7096342d3251
Ricardo Werner <wernerdive@gmail.com>
2026-08-12 10:33:48 -0300

    refactor(main): wrap app with ThemeProvider and router setup

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import "@/index.css";
 
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { ThemeProvider } from '@/context/theme/ThemeProvider.tsx'
 
 import App from "@/App";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  </StrictMode>
 );
